5f344d1b3410a3b948975ada173760135b467178
christopher-koleda <koleda.christopher@gmail.com>
2019-03-29 02:09:09 -0400

    Fix message styling

--- client/components/chat/Message.vue
+++ client/components/chat/Message.vue
@@ -1,17 +1,22 @@
 <template >
-  <div class="tw-flex tw-mb-6 ">
+  <div class="tw-flex tw-items-center tw-mb-8">
+
     <Avatar class="tw-mr-4"/>
 
-    <div class="tw-mt-1 tw-p-4 tw-bg-blue tw-text-white tw-rounded-lg tw-text-sm tw-w-3/5 xl:tw-w-1/3">
-      <div class="tw-mb-4">
+    <div 
+      class="tw-p-4 tw-bg-blue tw-rounded-lg tw-shadow tw-text-white tw-text-sm tw-w-3/5 xl:tw-w-1/3 tw-cursor-pointer" 
+      @click="details = !details">
+
+      <div>
         {{ data.body }}
-        <!-- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam nobis, enim fugiat mollitia similique voluptas maxime laboriosam officiis laborum, hic eum error obcaecati, ea magnam dolores voluptatibus. Ab, natus numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dicta iure at? Mollitia cum dignissimos corrupti nobis dolores est nesciunt non tempore ea nostrum, labore quod quas aspernatur assumenda impedit! -->
+        <!-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur laborum nihil, accusantium dicta corporis illum! Eaque, amet! Temporibus, hic asperiores? Eaque amet laborum eligendi hic error reiciendis explicabo modi facilis? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, officiis nihil veritatis similique praesentium quas natus libero explicabo illo id maxime vero culpa soluta magnam sit mollitia ipsa molestiae alias. -->
       </div>
-      <div class="tw-text-right tw-font-sans">
-        <span class="tw-mr-2">Sent: </span>
-        <span>{{ data.sent }}</span> 
+
+      <div v-show="details" class="tw-text-right tw-font-sans tw-mt-2">
+        <span class="tw-mr-1">Sent:</span>
+        <span>{{ data.sent ? data.sent : "2:04 am" }}</span> 
       </div>
-    </div>
+    </div><!-- end message bubble -->
 
   </div>
 </template>
@@ -29,9 +34,22 @@ export default {
       required: true
     }
   },
+
+  data() {
+    // TODO: Add an indicator for isLeft or isRight
+    // to display which side the message should be on 
+    // and switch the icon side accordingly (you can use flex order for this I believe or a v-if for simplicity)
+    return {
+      details : false
+    }
+  }
+
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  // .b-rad {
+  //   border-radius: 0px 12px 12px 10px;
+  // }
 </style>
+
